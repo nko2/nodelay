65fa537fde61d952cade236cbfb8ce16c5f5fe47
Will Munn <willmunn@hotmail.com>
2011-08-27 14:41:03 +0100

    added text to bubble

--- client/mindmap.js
+++ client/mindmap.js
@@ -11,9 +11,12 @@ function Mindmap(paper) {
 	this.defaultHeight = 50;
 };
 
+
 Mindmap.prototype.createBubble = function(x, y) {
 	var bubble = this.paper.ellipse(x, y, this.defaultWidth, this.defaultHeight);
 	bubble.attr(this.defaultBubbleAttributes);
+	this.addTextToBubble(bubble);
+
 	this.bubbles.push(bubble);
 	return bubble;
 };
@@ -22,6 +25,16 @@ Mindmap.prototype.connectBubbles = function(bubble1, bubble2) {
 	var connection = this.paper.connection(bubble1, bubble2, '#AECC75', '#AECC75');
 	this.connections.push(connection);
 	return connection;
+};
+
+Mindmap.prototype.addTextToBubble = function (bubble) {
+	var bubbleText = this.paper.text(
+			bubble.getBBox().x + this.defaultWidth/2, 
+			bubble.getBBox().y+ this.defaultHeight/2, 
+			"idea1"
+		).attr({fill : '#AECC75'});
+	bubble.text = bubbleText;
+	console.log(bubble);
 };
 
 module.exports = Mindmap;

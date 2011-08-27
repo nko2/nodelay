45831ee4d9fe0c2dc3e4f248715ff930d9b5170e
Antony Denyer <antonydenyer@hotmail.com>
2011-08-27 01:56:57 +0100

    syntax fix

--- client/index.js
+++ client/index.js
@@ -1,9 +1,8 @@
-var $ = require('jquery');
-	
 module.exports = {};
 
 $(function() {
 	alert("jquery works");
 	
-	var paper = Raphael(document.getElementById("scene",1024,768);
+	var paper = Raphael(document.getElementById("scene"),1024,768);
+	paper.circle(400,250,50);
 }

10339a2ccd63aa94d8b60cc0313ae3d2ffd1f77a
Will Munn <willmunn@hotmail.com>
2011-08-27 02:45:17 +0100

    draw square where clicked

--- client/index.js
+++ client/index.js
@@ -1,6 +1,8 @@
 $(function() {
-	alert("jquery works");
 
 	var paper = Raphael(document.getElementById("scene"),1024,768);
 	paper.circle(400,250,50);
+	$("#scene").click(function(evt){
+		paper.rect(evt.clientX, evt.clientY, 100, 100);
+	})
 });

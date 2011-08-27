d256b3c5c4c5bf8b67c31d6138a1a82c785f02d4
Will Munn <willmunn@hotmail.com>
2011-08-27 01:54:09 +0100

    fixed merge

--- client/index.js
+++ client/index.js
@@ -1,15 +1,9 @@
 var $ = require('jquery');
 	
 module.exports = {};
-<<<<<<< HEAD
-$(function(){
-	
-	alert("jquery works");
-=======
 
 $(function() {
 	alert("jquery works");
 	
 	var paper = Raphael(document.getElementById("scene",1024,768);
->>>>>>> cca7e03e33407047a931a247b64e6d5d600c5a32
 }

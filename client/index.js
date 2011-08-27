6fff8e69ee8a8166db35759558058ea31c09be87
Antony Denyer <antonydenyer@hotmail.com>
2011-08-27 16:47:44 +0100

    basic events

--- client/index.js
+++ client/index.js
@@ -7,7 +7,9 @@ $(function() {
 		pipes,
 		paper = Raphael(document.getElementById("scene"), width, height),
 		mindmap,
+		pipe,
 		Mindmap = require('./mindmap'),
+		Pipe = require('./pipe'),
 		User = require('./user');
 
 	mindmap = new Mindmap(paper);
@@ -24,5 +26,7 @@ $(function() {
 
 	user = new User();
 	user.setupUser();
+
+	pipe = new Pipe();
 });
 

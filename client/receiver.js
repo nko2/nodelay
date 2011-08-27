ec2dffc0014970cbb4007f3b765580eb51e75792
Antony Denyer <antonydenyer@hotmail.com>
2011-08-27 22:21:42 +0100

    receiver

--- client/receiver.js
+++ client/receiver.js
@@ -1,13 +1,8 @@
-var EventEmitter = require('events').EventEmitter;
 //this class shoudl take care of evets from outside and telling the
 //ui to updae
-function Pipe(mindmap, mindmapFacade) {
-	this.mindmap = mindmap;
+function Receiver(mindmap, mindmapFacade) {
 	this.mindmapFacade = mindmapFacade;
-};
-
-Pipe.prototype.wireUp = function() {
-	var mindmap = this.mindmap;
+	
 	now.receiveMoveEvent = function(name, bubble) {
 		if (name === now.name) return;
 
@@ -26,33 +21,7 @@ Pipe.prototype.wireUp = function() {
 
 		mindmap.connectBubbles(id1,id2);
 	};
-
-	this.mindmapFacade.on('bubble-added', added);
-	this.mindmapFacade.on('connection', fireConnection);
 };
-
-function added(data) {
-	now.bubbleAddedBroadcast({
-		id: data.bubble.id,
-		x: data.bubble.x,
-		y: data.bubble.y,
-		text: data.bubble.label
-	});
-
-	data.bubble.on('drag', broadcastMove);
-}
-
-function fireConnection(data) {
-	now.bubbleConnection(data.first.id, data.second.id);
-}
-
-function broadcastMove(data) {
-	now.moveBubble({
-		id: this.ellipse.id,
-		x: data.x,
-		y: data.y
-	});
-}
 
 function moveBubble(id, x, y) {
 	var bubble = this.mindmap.getBubble(id);

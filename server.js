c35a284e4c95e7a7fdba5c9670b04c183fe0c89f
tonyto <tonyto85@gmail.com>
2011-08-27 01:12:31 +0100

    created web project

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express'),
+	path = require('path'),
+	nowjs = require('now'),
+	browserify = require('browserify'),
+	server;
+
+server = express.createServer();
+
+server.configure(function configureAppAndMiddleware() {
+	server.set('view engine', 'jade');
+	server.set('view', path.join(__dirname, 'views'));
+	
+	server.use(express.bodyParser());
+	server.use(express.cookieParser());
+	server.use(express.static(path.join(__dirname, 'public')));
+	server.use(browserify({
+		require: path.join(__dirname, 'client/index')
+	}));
+});
+
+
+server.get('/', function showHomePage(req, res) {
+	res.render('index.jade');
+});
+
+server.listen(8080);
+
+console.log('Running on 8080');

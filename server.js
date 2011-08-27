05c6ad2f3fdbf05b92cb1989c7915d36a14c35e7
Antony Denyer <antonydenyer@hotmail.com>
2011-08-27 03:49:13 +0100

    roooooms

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require('express'),
 nko = require('nko')('LkbXCCkD9H9HndHX'),
 path = require('path'),
 nowjs = require('now'),
+eyes = require('eyes'),
 browserify = require('browserify'),
+crypto = require('crypto'),
 server;
 
 server = express.createServer();
+var rooms = [];
 
 server.configure(function configureAppAndMiddleware() {
 	server.set('view engine', 'jade');
@@ -23,13 +26,19 @@ server.get('/', function showHomePage(req, res) {
 	res.render('index.jade');
 });
 
-server.get('/workspace', function (req, res) {
+server.get('/workspace/:name', function(req, res) {
+	console.log(req.params.name);
 	res.render('workspace.jade');
 });
 
-server.post('/signup', function(req,res){
-	console.log(req.body.email);
-	res.redirect('/workspace');
+server.post('/create', function(req, res) {
+	if (rooms.indexOf(req.body.room) > 0) {
+		res.redirect('/');
+	}
+
+	console.log(req.params.room);
+	rooms.push(req.params.room);
+	res.redirect('/workspace/' + req.body.room);
 });
 server.listen(8080);
 
@@ -37,10 +46,14 @@ console.log('Running on 8080');
 
 var everyone = nowjs.initialize(server);
 nowjs.on('connect', function() {
-  console.log('joined: ' + this.now.name);
+	console.log('joined: ' + this.now.name);
 })
 
 nowjs.on('disconnect', function() {
-  console.log('left: ' + this.now.name);
+	console.log('left: ' + this.now.name);
+})
+
+nowjs.on('add', function() {
+	console.log('add: ' + this.now.message);
 })
 

04ca3d1eac8fff9f6acc763f1e3796157b814224
Raoul Millais <raoul@raoulmillais.com>
2011-08-28 16:27:32 +0100

    Split out socket server into new file

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express'),
 	nko = require('nko')('LkbXCCkD9H9HndHX'),
 	path = require('path'),
-	nowjs = require('now'),
 	eyes = require('eyes'),
 	_ = require('underscore'),
 	browserify = require('browserify'),
 	crypto = require('crypto'),
 	MindmapProvider = require('./lib/mindmap-provider'),
-	server, mindmapProvider;
+	SocketServer = require('./lib/socket-server'),
+	server, socketServer, mindmapProvider;
 
 mindmapProvider = new MindmapProvider();
 server = express.createServer();
@@ -61,7 +61,6 @@ server.post('/create', function(req, res) {
 				return mindmap.name == req.body.mindmapname;
 			});		
 
-		console.log('mindmap exists : ' + exists);
 		eyes.inspect(mindmaps);
 
 		if (exists) {
@@ -70,7 +69,6 @@ server.post('/create', function(req, res) {
 			res.redirect('/');
 		}
 		else {
-			console.log('mindmaps: ' + mindmaps);
 			provider.add({
 				name: req.body.mindmapname
 			});
@@ -80,88 +78,6 @@ server.post('/create', function(req, res) {
 });
 server.listen(8080);
 
-var everyone = nowjs.initialize(server);
-
-nowjs.on('connect', function() {
-	var self = this;
-	this.now.mindmap = 'monkey';
-
-	nowjs.getGroup(this.now.mindmap).addUser(this.user.clientId);
-	nowjs.getGroup(this.now.mindmap).now.userConnected(this.now.name);
-	console.log(this.now.name + ' joined: ' + this.now.mindmap);
-	
-	mindmapProvider.get(this.now.mindmap, function(err, mindmap) {
-		var bubble, connection;
-		
-		eyes.inspect(mindmap);
-
-		for (bubble in mindmap.bubbles) {
-			console.log('sending bubble: %j', bubble);
-			nowjs.getGroup(self.now.mindmap).now.receiveBubbleAdded(self.now.name, bubble);
-		}
-		
-		for(connection in mindmap.connections) {
-			console.log('sending connection: %j', connection);
-			nowjs.getGroup(self.now.mindmap).now.receiveBubbleConnection('SERVER', { first: connection.first, second: connection.second });
-		}
-	});
-});
-
-nowjs.on('disconnect', function() {
-	nowjs.getGroup(this.now.room).removeUser(this.user.clientId);
-	nowjs.getGroup(this.now.mindmap).now.userDisconnected(this.now.name);
-	console.log('left: ' + this.now.name);
-});
-
-everyone.now.bubbleMoveBroadcast = function(bubble) {
-	eyes.inspect(bubble);
-	nowjs.getGroup(this.now.mindmap).now.receiveBubbleMove(this.now.name, bubble);
-};
-
-everyone.now.bubbleAddedBroadcast = function(bubble) {
-	var self = this;
-	console.log('about to add bubble: %j', bubble);
-	mindmapProvider.addBubble(this.now.mindmap, bubble, function(err, result) {
-		var connection = { first: bubble.connectedBubbleId, second: bubble.id };
-		
-		console.log('about to add connection: %j', connection);
-		mindmapProvider.addConnection(self.now.mindmap, connection, function(err, result) {
-			if (err) {
-				console.info("Couldn't persist something :(")
-			} else {
-				console.info('Persisted all the things!');
-			}
-		});	
-	});
-
-	nowjs.getGroup(this.now.mindmap).now.receiveBubbleAdded(this.now.name, bubble);
-};
-
-everyone.now.bubbleConnectionBroadcast = function(id1, id2) {
-	var connection = { first: id1, second: id2 }
-	console.log('about to add connection: %j', connection);
-	mindmapProvider.addConnection(this.now.mindmap, connection, function(err, result) {
-		eyes.inspect(err);
-		eyes.inspect(result);
-	});
-	nowjs.getGroup(this.now.mindmap).now.receiveBubbleConnection(this.now.name, id1, id2);
-};
-
-
-everyone.now.bubbleDestroyedBroadcast = function(bubble) {
-	console.log('about to delete bubble: %j', bubble);
-	mindmapProvider.deleteBubble(this.now.mindmap, bubble, function(err, result) {	
-		if (err) {
-			console.info("Couldn't persist something :(")
-		} else {
-			console.info('deleted bubble!');
-		}
-	});
-
-	nowjs.getGroup(this.now.mindmap).now.receiveBubbleDestroyed(this.now.name, bubble.id);
-};
-
-everyone.now.bubbleLabelChangedBroadcast = function(id, text) {
-	nowjs.getGroup(this.now.mindmap).now.receiveBubbleLabelChanged(this.now.name, id, text);
-};
+socketServer = new SocketServer(server);
+socketServer.initialize();
 

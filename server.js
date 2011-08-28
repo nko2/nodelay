c5281656a17e4f8966eb00d06a6c794935203407
Raoul Millais <raoul@raoulmillais.com>
2011-08-28 16:19:24 +0100

    Added delete buble and fixed up some nowjs issues on the server

--- server.js
+++ server.js
@@ -81,21 +81,37 @@ server.post('/create', function(req, res) {
 server.listen(8080);
 
 var everyone = nowjs.initialize(server);
+
 nowjs.on('connect', function() {
+	var self = this;
 	this.now.mindmap = 'monkey';
-	
-	mindmapProvider.get(this.now.mindmap, function(err, result) {
-		eyes.inspect(result);
-	});
-	
+
+	nowjs.getGroup(this.now.mindmap).addUser(this.user.clientId);
 	nowjs.getGroup(this.now.mindmap).now.userConnected(this.now.name);
-	console.log('joined: ' + this.now.id);
-})
+	console.log(this.now.name + ' joined: ' + this.now.mindmap);
+	
+	mindmapProvider.get(this.now.mindmap, function(err, mindmap) {
+		var bubble, connection;
+		
+		eyes.inspect(mindmap);
+
+		for (bubble in mindmap.bubbles) {
+			console.log('sending bubble: %j', bubble);
+			nowjs.getGroup(self.now.mindmap).now.receiveBubbleAdded(self.now.name, bubble);
+		}
+		
+		for(connection in mindmap.connections) {
+			console.log('sending connection: %j', connection);
+			nowjs.getGroup(self.now.mindmap).now.receiveBubbleConnection('SERVER', { first: connection.first, second: connection.second });
+		}
+	});
+});
 
 nowjs.on('disconnect', function() {
+	nowjs.getGroup(this.now.room).removeUser(this.user.clientId);
 	nowjs.getGroup(this.now.mindmap).now.userDisconnected(this.now.name);
 	console.log('left: ' + this.now.name);
-})
+});
 
 everyone.now.bubbleMoveBroadcast = function(bubble) {
 	eyes.inspect(bubble);
@@ -103,22 +119,24 @@ everyone.now.bubbleMoveBroadcast = function(bubble) {
 };
 
 everyone.now.bubbleAddedBroadcast = function(bubble) {
+	var self = this;
 	console.log('about to add bubble: %j', bubble);
 	mindmapProvider.addBubble(this.now.mindmap, bubble, function(err, result) {
 		var connection = { first: bubble.connectedBubbleId, second: bubble.id };
 		
 		console.log('about to add connection: %j', connection);
-		mindmapProvider.addConnection(this.now.mindmap, connection, function(err, result) {
+		mindmapProvider.addConnection(self.now.mindmap, connection, function(err, result) {
 			if (err) {
 				console.info("Couldn't persist something :(")
 			} else {
-				console('Persisted all the things!');
+				console.info('Persisted all the things!');
 			}
 		});	
 	});
 
 	nowjs.getGroup(this.now.mindmap).now.receiveBubbleAdded(this.now.name, bubble);
 };
+
 everyone.now.bubbleConnectionBroadcast = function(id1, id2) {
 	var connection = { first: id1, second: id2 }
 	console.log('about to add connection: %j', connection);
@@ -130,8 +148,17 @@ everyone.now.bubbleConnectionBroadcast = function(id1, id2) {
 };
 
 
-everyone.now.bubbleDestroyedBroadcast = function(id) {
-	nowjs.getGroup(this.now.mindmap).now.receiveBubbleDestroyed(this.now.name, id);
+everyone.now.bubbleDestroyedBroadcast = function(bubble) {
+	console.log('about to delete bubble: %j', bubble);
+	mindmapProvider.deleteBubble(this.now.mindmap, bubble, function(err, result) {	
+		if (err) {
+			console.info("Couldn't persist something :(")
+		} else {
+			console.info('deleted bubble!');
+		}
+	});
+
+	nowjs.getGroup(this.now.mindmap).now.receiveBubbleDestroyed(this.now.name, bubble.id);
 };
 
 everyone.now.bubbleLabelChangedBroadcast = function(id, text) {
